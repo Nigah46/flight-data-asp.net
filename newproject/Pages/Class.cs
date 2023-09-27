175b7bd7b11c01ee182781e0480abbf8d43986ad
unknown <nigahmakk@outlook.com>
2023-09-27 09:59:30 +0530

    fixes

---
+++ newproject/Pages/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace newproject.Pages
+{
+    public class CustomerInfo
+    {
+        public string id;
+        public string name;
+        public string address;
+        public string contact;
+        public string destination;
+    }
+}
---
+++ newproject/Pages/Create.cshtml.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace newproject.Pages
+{
+    public class CreateModel : PageModel
+        // configuarations for connecting with data source
+    {
+        private readonly string connectionString;
+        public CreateModel(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        
+
+
+
+        public CustomerInfo customerInfo = new();
+        public string errormessage = "";
+        public string successmessage = "";
+
+
+
+
+
+
+        public void OnPost()
+        {
+            customerInfo.id = Request.Form["id"];
+            customerInfo.name = Request.Form["name"];
+            customerInfo.address = Request.Form["address"];
+            customerInfo.contact = Request.Form["contact"];
+            customerInfo.destination = Request.Form["destination"];
+            try
+            {
+                using SqlConnection connection = new SqlConnection(connectionString);
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO customers" +
+                        "(id , name , contact ,address, destination) VALUES" +
+                        "(@id , @name , @contact , @address , @destination);";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", customerInfo.id);
+                        command.Parameters.AddWithValue("@name", customerInfo.name);
+                        command.Parameters.AddWithValue("@contact", customerInfo.contact);
+                        command.Parameters.AddWithValue("@address", customerInfo.address);
+                        command.Parameters.AddWithValue("@destination", customerInfo.destination);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                errormessage = ex.Message;
+
+            }
+            //Response.Redirect("/login");
+            successmessage = "your data has been accepted ";
+
+            customerInfo.name = "";
+
+            
+        }
+
+    }
+}
